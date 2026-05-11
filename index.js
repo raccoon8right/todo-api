60083c46aada787c5d51ab40fe319e3f9e084cd3
raccoon8right <gpepito408@gmail.com>
2026-05-10 21:58:25 -0400

    feat: add CORS support for frontend connection

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ import express from 'express'
 import 'dotenv/config'
 import authRoutes from './src/routes/authRoutes.js'
 import tareasRoutes from './src/routes/tareasRoutes.js'
+import cors from 'cors'
 
 const app = express()
+app.use(cors({
+    origin: 'http://localhost:5173'
+}))
 app.use(express.json())
 
 app.use('/auth', authRoutes)
