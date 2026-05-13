d0f0c691e0ddf76df548b036aff8a8ac69e2f326
raccoon8right <gpepito408@gmail.com>
2026-05-13 02:37:47 -0400

    debug: log enviroment variables on startup

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import express from 'express'
+console.log('Variables de entorno:', {
+    DB_HOST: process.env.DB_HOST,
+    DB_USER: process.env.DB_USER,
+    DB_NAME: process.env.DB_NAME,
+    DB_PORT: process.env.DB_PORT,
+    PORT: process.env.PORT
+})
 import 'dotenv/config'
 import authRoutes from './src/routes/authRoutes.js'
 import tareasRoutes from './src/routes/tareasRoutes.js'
